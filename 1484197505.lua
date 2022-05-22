9cbf3a0b56752f6a78096a17bd43c6556b6fb254
ClipCorp <105748701+ClipCorp@users.noreply.github.com>
2022-05-22 00:34:20 -0300

    Update 1484197505.lua

--- 1484197505.lua
+++ 1484197505.lua
@@ -3,26 +3,23 @@ function Reselect()
 		local Highlight = Instance.new("Highlight",game:GetService("Workspace").CurrentCamera)
 		Highlight.FillTransparency = 1
 		Highlight.Adornee = v.Character
+		Highlight.Name = v.DisplayName
 	end
 end
 
 Reselect()
 
-game:GetService("Players").PlayerAdded:Connect(function(player)
+game:GetService("Players").ChildAdded:Connect(function(player)
 	for i,v in pairs(game:GetService("Workspace").CurrentCamera:GetChildren()) do
 		if v:IsA("Highlight") then
 			v:Destroy()
 		end
 	end
-	Reselect()
+    Reselect()
 end)
 
-game:GetService("Players").PlayerRemoving:Connect(function(player)
-	for i,h in pairs(game:GetService("Workspace").CurrentCamera:GetChildren()) do
-		if h:IsA("Highlight") and h.Adornee == player.Character then
-			h:Destroy()
-		end
-	end
+game:GetService("Players").ChildRemoved:Connect(function(player)
+	game:GetService("Workspace").CurrentCamera:WaitForChild(player.DisplayName):Destroy()
 end)
 
 while wait() do
@@ -33,22 +30,20 @@ while wait() do
 					h.OutlineColor = Color3.fromRGB(255, 56, 56)
 				end
 			end
-		else 
+		end
 			if v.Status.Role.Value == "Bystander" and v.Status.HasRevolver.Value == true then
 				for i,h in pairs(game:GetService("Workspace").CurrentCamera:GetChildren()) do
 					if h:IsA("Highlight") and h.Adornee == v.Character then
 						h.OutlineColor = Color3.fromRGB(88, 152, 255)
 					end
 				end
-			else
+			end
 				if v.Status.Role.Value == "Bystander" and v.Status.HasRevolver.Value == false then
 					for i,h in pairs(game:GetService("Workspace").CurrentCamera:GetChildren()) do
 						if h:IsA("Highlight") and h.Adornee == v.Character then
-							h.OutlineColor = Color3.fromRGB(255, 255, 255)
+							h.OutlineColor = Color3.fromRGB(90, 255, 78)
 						end
 					end
 				end
 			end
-		end
-	end
 end
